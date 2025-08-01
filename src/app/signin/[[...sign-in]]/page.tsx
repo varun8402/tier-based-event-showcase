7a89ab5abe1edf202febd4c5f617776d37edf2bd
varun8402 <varunmarwah.2004@gmail.com>
2025-08-01 13:12:19 +0530

    added sign-in using clerk

---
+++ src/app/signin/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <SignIn 
+        forceRedirectUrl="/"
+        fallbackRedirectUrl="/"
+      />
+    </div>
+  )
+}
